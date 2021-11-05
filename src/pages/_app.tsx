037330de122580d19cdb2579dffef11dafb3c733
inaridiy <inainainariinainari@gmail.com>
2021-11-05 16:18:36 +0900

    next chakra UI tsの設定

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <ChakraProvider>{getLayout(<Component {...pageProps} />)}</ChakraProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from 'react';
+import { Flex, Heading } from '@chakra-ui/react';
+
+export default function Home() {
+  return <Heading>Test</Heading>;
+}
