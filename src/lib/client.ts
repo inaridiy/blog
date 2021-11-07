6d5a248f260e1e0f528f7d40d58fa6186ebbfa25
inaridiy <inainainariinainari@gmail.com>
2021-11-08 00:15:10 +0900

    デバッグメッセージ追加

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,7 @@
 import { createClient } from 'microcms-js-sdk';
 
+console.log(process.env.SERVICE_DOMAIN, process.env.API_KEY);
+
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN as string,
   apiKey: process.env.API_KEY as string,
