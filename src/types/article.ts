89be9b960b1e897058c9342a1281151d1d7cb596
inaridiy <inainainariinainari@gmail.com>
2021-11-08 14:37:57 +0900

    OGP画像系の型追加

--- src/types/article.ts
+++ src/types/article.ts
@@ -7,6 +7,7 @@ export type Article = {
   category: [];
   title: string;
   body: string;
+  og_frame: OGFrame;
 };
 
 export type ArticleList = {
@@ -24,4 +25,26 @@ export type Category = {
   publishedAt: string;
   revisedAt: string;
   name: string;
+};
+
+export type OGFrame = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  keyWord: string;
+  theme: 'light' | 'dark';
+  images: UploadImage[];
+};
+
+export type UploadImage = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+  image: string;
 };
