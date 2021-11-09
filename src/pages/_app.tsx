5ef0b848be05534420032d356eaef987997644a7
inaridiy <inainainariinainari@gmail.com>
2021-11-09 23:43:01 +0900

    twitterの設定を追加2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,8 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         <title>{process.env.NEXT_PUBLIC_SITE_NAME}</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <meta name="description" content="無名な学生のしがないブログ" />
+        <meta name="keywords" content="無名,学生,ブログ" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <ChakraProvider>{getLayout(<Component {...pageProps} />)}</ChakraProvider>
     </>
