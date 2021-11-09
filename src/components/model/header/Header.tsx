93486bc69b207760a7404f842803d2b2b3e6216f
inaridiy <inainainariinainari@gmail.com>
2021-11-10 00:10:56 +0900

    ヘッダーを修正

--- src/components/model/header/Header.tsx
+++ src/components/model/header/Header.tsx
@@ -5,6 +5,7 @@ import {
   Text,
   IconButton,
   Link,
+  Box,
 } from '@chakra-ui/react';
 import { BsTwitter } from 'react-icons/bs';
 import { ToggleColorBtn } from '../../ui/buttons/toggleLightMode';
@@ -16,21 +17,23 @@ type Props = {};
 export const Header: React.FC<Props> = ({}) => {
   const twitter = process.env.NEXT_PUBLIC_TWITTER;
   return (
-    <Container maxW={'container.xl'} pos="fixed" zIndex={5}>
-      <HStack bg="transparent" h="24">
-        <HeaderTitle />
-        <Spacer />
-        <Link href={twitter} isExternal>
-          <IconButton
-            icon={<BsTwitter />}
-            aria-label="Toggle menu"
-            variant="ghost"
-            size="lg"
-            to={twitter}
-          />
-        </Link>
-        <ToggleColorBtn />
-      </HStack>
-    </Container>
+    <Box pos="fixed" w="full">
+      <Container maxW={'container.xl'} zIndex={5}>
+        <HStack bg="transparent" h="24">
+          <HeaderTitle />
+          <Spacer />
+          <Link href={twitter} isExternal>
+            <IconButton
+              icon={<BsTwitter />}
+              aria-label="Toggle menu"
+              variant="ghost"
+              size="lg"
+              to={twitter}
+            />
+          </Link>
+          <ToggleColorBtn />
+        </HStack>
+      </Container>
+    </Box>
   );
 };
