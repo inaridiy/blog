8693e8bd071b51edbe8c2e4185fabb4155d6e84e
inaridiy <inainainariinainari@gmail.com>
2021-11-11 22:33:24 +0900

    GA4導入

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { Box } from '@chakra-ui/layout';
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || '';
+
 class MyDocument extends Document {
   render() {
     return (
@@ -10,6 +12,24 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap"
             rel="stylesheet"
           />
+          {GA_TRACKING_ID && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}');
+                  `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <Box as="body" fontFamily="'Noto Sans JP', sans-serif">
           <Main />
