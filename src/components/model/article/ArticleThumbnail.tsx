0296b4da88f5913bc949694363f63ef6772766d0
inaridiy <inainainariinainari@gmail.com>
2021-12-12 01:48:42 +0900

    画像系の最適化

--- src/components/model/article/ArticleThumbnail.tsx
+++ src/components/model/article/ArticleThumbnail.tsx
@@ -7,14 +7,13 @@ type Props = { article: Article };
 export const ArticleThumbnail: React.FC<Props> = ({ article }) => {
   const ogImage = useOgImage(article);
   return (
-    <div className="overflow-hidden rounded-lg drop-shadow-lg">
+    <div className="overflow-hidden drop-shadow-lg">
       <Image
+        className="rounded-lg"
         src={ogImage}
         alt={article.title}
-        layout="responsive"
         width="1024"
         height="585"
-        objectFit="cover"
       />
     </div>
   );
--- src/lib/transpiler.ts
+++ src/lib/transpiler.ts
@@ -6,7 +6,8 @@ export const mdToHTML = (markdown: string) => {
   const html = markdownToHtml(markdown);
   const result = html
     .replace(/<p>(.*?)<\/p>/gs, pReplacer)
-    .replace(/<h(\d) (.*?)>(.*?)<\/h\d>/gs, heddingReplacer);
+    .replace(/<h(\d) (.*?)>(.*?)<\/h\d>/gs, heddingReplacer)
+    .replace(/<img (.*?)src="(.*?)" (.*?)>/gs, imageReplacer);
   return result;
 };
 //
@@ -17,5 +18,10 @@ const pReplacer = (match: string, p1: string, p2: string) => {
 
 const heddingReplacer = (match: string, p1: string, p2: string, p3: string) => {
   const result = `<h${p1} ${p2}>${parser.translateHTMLString(p3)}</h${p1}>`;
+  return result;
+};
+
+const imageReplacer = (match: string, p1: string, p2: string, p3: string) => {
+  const result = `<img loading="lazy" ${p1} src="${p2}?h=480" ${p3} />`;
   return result;
 };
