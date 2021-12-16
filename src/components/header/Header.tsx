9eb2a5cc032f350aad047308d50421ab13a70912
inaridiy <inainainariinainari@gmail.com>
2021-12-16 23:09:22 +0900

    ヘッダーを修正

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,12 +4,10 @@ import { ToggleLightMode } from '../ui';
 
 export const Header: React.FC = ({}) => {
   return (
-    <header className="fixed z-10 mx-auto w-full h-16">
-      <div className="container flex items-center px-2 sm:px-4 h-full">
-        <HeaderTitle />
-        <Spacer />
-        <ToggleLightMode />
-      </div>
+    <header className="container flex fixed z-10 items-center px-2 sm:px-4 mx-auto w-full h-16">
+      <HeaderTitle />
+      <Spacer />
+      <ToggleLightMode />
     </header>
   );
 };
