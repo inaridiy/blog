ab9b162bdaa6c2915800ff90447860bd0ef1f43d
inaridiy <inainainariinainari@gmail.com>
2021-12-17 21:40:57 +0900

    ちょい修正

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,7 +6,7 @@ type Props = {};
 export const About: React.FC<Props> = ({}) => {
   return (
     <div
-      className={`max-w-screen-lg flex flex-col sm:flex-row gap-3 sm:items-center mt-8 
+      className={`max-w-screen-md flex flex-col sm:flex-row gap-3 sm:items-center mt-8 
       bg-gradient-to-bl from-cyan-300 to-pink-500 rounded-lg 
       border-2 dark:border-gray-700  drop-shadow-lg`}
     >
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import { ToggleLightMode } from '../ui';
 export const Header: React.FC = ({}) => {
   return (
     <header className="flex fixed z-10 justify-center w-full drop-shadow-lg">
-      <div className="container flex items-center px-2 sm:px-0 pr-4 h-16">
+      <div className="container flex items-center pr-4 mx-0 sm:mx-4 h-16">
         <HeaderTitle />
         <Spacer />
         <ToggleLightMode />
