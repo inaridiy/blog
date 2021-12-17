c0ec7f5445b8b470fead4b10976c13253cb90d1c
inaridiy <inainainariinainari@gmail.com>
2021-12-17 21:26:36 +0900

    ダークモードをキャッシュするように

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { ThemeValue, SetTheme } from '../types/theme';
+import { useLocalStorage } from 'react-use';
 
 export const themeValueContext = createContext<ThemeValue>({ mode: 'light' });
 export const setThemeContext = createContext<SetTheme>(() => undefined);
@@ -8,6 +9,20 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [theme, setTheme] = useState<ThemeValue>({ mode: 'light' });
+  const [storageValue, setStorage] = useLocalStorage<'dark' | 'light'>('theme');
+
+  useEffect(() => {
+    storageValue ||
+      setStorage(
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light'
+      );
+    setTheme({
+      ...theme,
+      mode: storageValue ?? 'light',
+    });
+  }, []);
 
   useEffect(() => {
     if (theme.mode === 'dark') {
@@ -15,6 +30,8 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
     } else {
       document.documentElement.classList.remove('dark');
     }
+    setStorage(theme.mode);
+    console.log(storageValue);
   }, [theme]);
   return (
     <themeValueContext.Provider value={theme}>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import initTwitterScriptInner from 'zenn-embed-elements/lib/init-twitter-script-inner';
+import { Header } from '../components/header';
+import { ThemeProvider } from '../components/ThemeProvider';
+
 import 'tailwindcss/tailwind.css';
 import '../assets/toc.css';
 import '../assets/Article.scss';
-import { Header } from '../components/header';
-import { ThemeProvider } from '../components/ThemeProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => import('zenn-embed-elements') as any, []);
@@ -17,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           __html: initTwitterScriptInner,
         }}
       />
-      <div className="flex overflow-hidden relative flex-col min-w-full min-h-screen bg-trueGray-100 dark:bg-trueGray-800">
+      <div className="flex overflow-hidden relative flex-col min-w-full min-h-screen text-gray-900 dark:text-white bg-trueGray-100 dark:bg-trueGray-800">
         <Header />
         <main className="relative flex-grow mt-16">
           <Component {...pageProps} />
