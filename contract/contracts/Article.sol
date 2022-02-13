6145b940c48eb0959057157c037dd7da9d53797d
inaridiy <inainainariinainari@gmail.com>
2022-02-13 20:54:36 +0900

    スマコンとテストかいたぁ

--- contract/contracts/Article.sol
+++ contract/contracts/Article.sol
@@ -5,8 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "hardhat/console.sol";
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.4;
 
 contract Article is ERC721URIStorage, AccessControlEnumerable {
     bytes32 public constant WRITER_ROLE = keccak256("Writer");
@@ -21,7 +22,8 @@ contract Article is ERC721URIStorage, AccessControlEnumerable {
     function post(string memory tokenURI) public returns (uint256) {
         require(
             hasRole(WRITER_ROLE, msg.sender) ||
-                hasRole(EDITOR_ROLE, msg.sender),
+                hasRole(EDITOR_ROLE, msg.sender) ||
+                hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
             "You don't have the authority."
         );
         _tokenIds.increment();
@@ -31,18 +33,23 @@ contract Article is ERC721URIStorage, AccessControlEnumerable {
         return newItemId;
     }
 
-    function edit(string memory tokenURI, uint256 tokenId) public {
+    function edit(uint256 tokenId, string memory tokenURI) public {
         require(
-            hasRole(EDITOR_ROLE, msg.sender),
+            hasRole(EDITOR_ROLE, msg.sender) ||
+                hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
             "You don't have the authority."
         );
+        require(isOwnerdByMembers(tokenId), "A third party has the token.");
         _setTokenURI(tokenId, tokenURI);
     }
 
-    function isOwnerdByMembers(uint256 tokenId) public view {
-        bool result = false;
+    function isOwnerdByMembers(uint256 tokenId) public view returns (bool) {
         address tokenOwner = ownerOf(tokenId);
-        uint256 writerCount = getRoleMemberCount(WRITER_ROLE);
+        bool isAdmin = hasRole(DEFAULT_ADMIN_ROLE, tokenOwner);
+        bool isEditor = hasRole(EDITOR_ROLE, tokenOwner);
+        bool isWriter = hasRole(WRITER_ROLE, tokenOwner);
+        //    console.log(tokenOwner);
+        return isAdmin || isEditor || isWriter;
     }
 
     function supportsInterface(bytes4 interfaceId)
--- contract/test/index.ts
+++ contract/test/index.ts
@@ -1,5 +1,8 @@
-import { expect } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import chai, { expect } from "chai";
 import { ethers } from "hardhat";
+import { Article } from "../typechain";
+chai.use(require("chai-as-promised"));
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
@@ -15,5 +18,102 @@ describe("Greeter", function () {
     await setGreetingTx.wait();
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
+
+const deployArticle = async () => {
+  const Article = await ethers.getContractFactory("Article");
+  const article = await Article.deploy();
+  await article.deployed();
+  return article;
+};
+
+let article: Article;
+let addrs: SignerWithAddress[];
+
+const ADMIN_ROLE = `0x${"0".repeat(64)}`;
+const WRITER_ROLE = ethers.utils.id("Writer");
+const EDITOR_ROLE = ethers.utils.id("Editor");
+
+describe("Article", function () {
+  beforeEach(async function () {
+    article = await deployArticle();
+    addrs = await ethers.getSigners();
+  });
+
+  it("post uri", async function () {
+    await article.post("Hello World");
+    expect(await article.tokenURI(1)).to.equal("Hello World");
+    await article.post("Hello World2");
+    expect(await article.tokenURI(2)).to.equal("Hello World2");
+  });
+
+  it("check admin role", async function () {
+    expect(await article.getRoleMember(ADMIN_ROLE, 0)).to.equal(
+      await article.signer.getAddress()
+    );
+  });
+
+  it("Whether you can't Mint without a role.", async function () {
+    const [owner, addr1] = addrs;
+
+    await expect(article.connect(addr1).post("Hello World")).to.rejectedWith(
+      Error
+    );
+  });
+
+  it("grant role", async function () {
+    const [owner, addr1] = addrs;
+    await article.grantRole(WRITER_ROLE, addr1.address);
+    expect(await article.getRoleMember(WRITER_ROLE, 0)).to.equal(addr1.address);
+  });
+
+  it("post uri with role", async function () {
+    const [owner, writer, editor] = addrs;
+    await article.grantRole(WRITER_ROLE, writer.address);
+    await article.connect(writer).post(`Hello World with ${writer.address}`);
+    expect(await article.tokenURI(1)).to.equal(
+      `Hello World with ${writer.address}`
+    );
+
+    await article.grantRole(EDITOR_ROLE, editor.address);
+    await article.connect(editor).post(`Hello World with ${editor.address}`);
+    expect(await article.tokenURI(2)).to.equal(
+      `Hello World with ${editor.address}`
+    );
+  });
+
+  it("No third party can edit it.", async function () {
+    const [owner, thirdPerson, writer] = addrs;
+    await article.post("Test Post");
+    await expect(
+      article.connect(thirdPerson).edit(1, "I'm gonna tamper with it.")
+    ).to.rejectedWith(Error);
+
+    await article.grantRole(WRITER_ROLE, writer.address);
+    await expect(
+      article.connect(writer).edit(1, "I'm gonna tamper with it.")
+    ).to.rejectedWith(Error);
+  });
+
+  it("edit with editor", async function () {
+    const [owner, editor] = addrs;
+    await article.post("Test Post");
+    await article.grantRole(EDITOR_ROLE, editor.address);
+    await article.connect(editor).edit(1, "I'm gonna tamper with it.");
+    expect(await article.tokenURI(1)).to.equal("I'm gonna tamper with it.");
+  });
+
+  it("Cannot be edited once transferred", async function () {
+    const [owner, thirdPerson] = addrs;
+    await article.post("Test Post");
+    await article.edit(1, "I'm gonna tamper with it.");
+    expect(await article.tokenURI(1)).to.equal("I'm gonna tamper with it.");
+    await article.transferFrom(
+      await article.signer.getAddress(),
+      thirdPerson.address,
+      1
+    );
+    await expect(article.edit(1, "I cant tamper...")).to.rejectedWith(Error);
   });
 });
