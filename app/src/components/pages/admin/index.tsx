84761b241f7da66006c0c056bb819724183d3117
inaridiy <inainainariinainari@gmail.com>
2022-02-20 11:31:44 +0900

    Adminにユーザー一覧を表示できるように

--- app/src/components/pages/admin/index.tsx
+++ app/src/components/pages/admin/index.tsx
@@ -1,9 +1,19 @@
 import { Box, FullLoading } from "@/components/base";
+import { AccountInfo } from "@/components/modules/account";
 import { StyledIconButton } from "@/components/modules/buttons/styles";
 import { useContent, useContract, useWeb3 } from "@/hooks";
-import { getArticles } from "@/util/AdminUtil";
-import { Button, Card, Row, Spacer, Text } from "@nextui-org/react";
-import { useEffect } from "react";
+import { useContractFetcher } from "@/hooks/useFetcher";
+import { Account } from "@/types/web3Types";
+import { getMembers } from "@/util/AdminUtil";
+import {
+  Button,
+  Card,
+  Container,
+  Loading,
+  Row,
+  Spacer,
+  Text,
+} from "@nextui-org/react";
 import { BsPlus } from "react-icons/bs";
 
 const AdminPage: React.FC = () => {
@@ -58,12 +68,6 @@ const AdminPage: React.FC = () => {
 export default AdminPage;
 
 const AdminArticles: React.FC = () => {
-  const contract = useContract();
-  useEffect(() => {
-    if (contract) {
-      getArticles(contract).catch((e) => console.error(e));
-    }
-  }, [contract]);
   return (
     <Card>
       <Row justify="space-between" align="center">
@@ -79,16 +83,44 @@ const AdminArticles: React.FC = () => {
 };
 
 const AdminMembers: React.FC = () => {
+  const contract = useContract();
+  const { data } = useContractFetcher(contract, getMembers);
   return (
     <Card>
       <Row justify="space-between" align="center">
         <Text size={30} b>
           メンバー一覧
+          {!data && <Loading />}
         </Text>
         <StyledIconButton>
           <BsPlus size="3em" />
         </StyledIconButton>
       </Row>
+      {data && (
+        <>
+          <AdminShowMembers text="Admin" accounts={data.admin} />
+          <AdminShowMembers text="Editor" accounts={data.editor} />
+          <AdminShowMembers text="Writer" accounts={data.writer} />
+        </>
+      )}
     </Card>
+  );
+};
+
+const AdminShowMembers: React.FC<{
+  text: string;
+  accounts: Account[];
+}> = ({ text, accounts }) => {
+  return accounts.length ? (
+    <Container>
+      <Text size={24} b>
+        {text}
+      </Text>
+      {accounts.map((account) => (
+        <AccountInfo account={account} size="large" />
+      ))}
+    </Container>
+  ) : (
+    <></>
   );
 };
--- app/src/util/AdminUtil.ts
+++ app/src/util/AdminUtil.ts
@@ -1,7 +1,33 @@
 import { Articles } from "./contracts";
+import { convertAccount } from "./web3Util";
 
 export const getArticles = async (contract: Articles) => {
   const signerAddress = await contract.signer.getAddress();
   const res = await contract.WRITER_ROLE();
   console.log(res);
+};
+
+export const getMembers = async (contract: Articles) => {
+  const [admin, editor, writer] = await Promise.all([
+    (async () =>
+      await getRoleMembers(contract, await contract.DEFAULT_ADMIN_ROLE()))(),
+    (async () =>
+      await getRoleMembers(contract, await contract.EDITOR_ROLE()))(),
+    (async () =>
+      await getRoleMembers(contract, await contract.WRITER_ROLE()))(),
+  ]);
+  console.log({ admin, editor, writer });
+  return { admin, editor, writer };
+};
+
+export const getRoleMembers = async (contract: Articles, role: string) => {
+  const roleMemberCount = await contract.getRoleMemberCount(role);
+  const roleMembersPromise = Array(roleMemberCount.toNumber())
+    .fill("")
+    .map(
+      async (_, index) =>
+        await convertAccount(await contract.getRoleMember(role, index))
+    );
+  const roleMembers = await Promise.all(roleMembersPromise);
+  return roleMembers;
 };
